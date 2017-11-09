c30759bbcc714a3bcd58edb97d70e73d1362b143
ZephireNZ <brynleymcdonald@gmail.com>
2017-11-09 21:13:37 +1300

    Move SharePoint parsing/URLs to the KBBackend

--- src/main/java/nz/zephire/exceptions/postback/SharePointPostback.java
+++ src/main/java/nz/zephire/exceptions/postback/SharePointPostback.java
@@ -1,34 +1,16 @@
 package nz.zephire.exceptions.postback;
 
 import com.google.common.base.Joiner;
-import com.google.gson.Gson;
 import nz.zephire.exceptions.postback.backend.KBBackend;
 import nz.zephire.exceptions.postback.backend.SPBackend;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SharePointPostback implements SPPostbackObject {
 
-    // URL to GET, provides the initial setup of the form
-    public static final String INIT_URL = "http://2dintranet.2degreesmobile.co.nz/tools/requests/_layouts/FormServer.aspx?"
-            + "XsnLocation=http://2dintranet.2degreesmobile.co.nz/tools/requests/Exceptions/Forms/template.xsn"
-            + "&SaveLocation=http%3A%2F%2F2dintranet%2E2degreesmobile%2Eco%2Enz%2Ftools%2Frequests%2FExceptions";
 
-    // URL to send the form to once complete
-    public static final String POSTBACK_URL = "https://2dintranet.2degreesmobile.co.nz/tools/requests/_layouts/Postback.FormServer.aspx";
-
-    // Locations of important parts in currentFormData object
-    private static final int REQUEST_UUID = 3;
-    private static final int TIMESTAMP_ZEROED = 18;
-    private static final int CANARY = 37;
-
-    // Described path through tree to find important parts
-    private static final int[] APPROVERS_UNWRAP_SEQUENCE = new int[] {1, 1, 0, 0, 1, 2, 2, 1, 2};
     private final SPBackend backend;
 
 
@@ -64,41 +46,7 @@ public class SharePointPostback implements SPPostbackObject {
         return Joiner.on(" ").join(objects);
     }
 
-    /**
-     * Finds the JSON variable representing form state, and returns it
-     * @param html Unencoded HTML body
-     * @return {@link SPFormData} representing important info in state
-     */
-    public static SPFormData parseFormData(String html) {
-        Pattern regex = Pattern.compile("var g_objCurrentFormData_FormControl = (.*);");
-        Matcher match = regex.matcher(html);
 
-        return readFormData(match.group(1));
-    }
 
-    // Parses "JSON of var g_objCurrentFormData_FormControl"
-    private static SPFormData readFormData(String formData) {
-        List<?> obj = new Gson().fromJson(formData, List.class);
 
-        // Navigates the nested arrays using given sequence
-        List<?> curr = obj;
-        for(int i : APPROVERS_UNWRAP_SEQUENCE) {
-            curr = (List<?>) curr.get(i);
-        }
-
-        List<String> approvers = new ArrayList<>(curr.size());
-        for(Object approver : curr) {
-            if(!(approver instanceof String)) continue;
-
-            approvers.add((String) approver);
-        }
-
-        return new SPFormData(
-                (String) obj.get(REQUEST_UUID),
-                (String)obj.get(TIMESTAMP_ZEROED),
-                (String) obj.get(CANARY),
-                approvers,
-                obj
-        );
-    }
 }
--- src/main/java/nz/zephire/exceptions/postback/backend/KBBackend.kt
+++ src/main/java/nz/zephire/exceptions/postback/backend/KBBackend.kt
@@ -2,14 +2,68 @@ package nz.zephire.exceptions.postback.backend
 
 import com.google.common.base.Joiner
 import com.google.common.io.CharStreams
+import com.google.gson.Gson
 import nz.zephire.exceptions.Utils
 import nz.zephire.exceptions.postback.SPFormData
-import nz.zephire.exceptions.postback.SharePointPostback
-import nz.zephire.exceptions.postback.SharePointPostback.INIT_URL
-import nz.zephire.exceptions.postback.SharePointPostback.POSTBACK_URL
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.*
+import java.util.*
+import java.util.regex.Pattern
+
+// URL to GET, provides the initial setup of the form
+private const val INIT_URL = "http://2dintranet.2degreesmobile.co.nz/tools/requests/_layouts/FormServer.aspx?" +
+        "XsnLocation=http://2dintranet.2degreesmobile.co.nz/tools/requests/Exceptions/Forms/template.xsn" +
+        "&SaveLocation=http%3A%2F%2F2dintranet%2E2degreesmobile%2Eco%2Enz%2Ftools%2Frequests%2FExceptions"
+
+// URL to send the form to once complete
+private const val POSTBACK_URL = "https://2dintranet.2degreesmobile.co.nz/tools/requests/_layouts/Postback.FormServer.aspx"
+
+// Locations of important parts in currentFormData object
+private const val REQUEST_UUID = 3
+private const val TIMESTAMP_ZEROED = 18
+private const val CANARY = 37
+
+// Described path through tree to find important parts
+val APPROVERS_UNWRAP_SEQUENCE = intArrayOf(1, 1, 0, 0, 1, 2, 2, 1, 2)
+
+/**
+ * Finds the JSON variable representing form state, and returns it
+ * @param html Unencoded HTML body
+ * @return [SPFormData] representing important info in state
+ */
+fun parseFormData(html: String): SPFormData {
+    val regex = Pattern.compile("var g_objCurrentFormData_FormControl = (.*);")
+    val match = regex.matcher(html)
+
+    return readFormData(match.group(1))
+}
+
+// Parses "JSON of var g_objCurrentFormData_FormControl"
+private fun readFormData(formData: String): SPFormData {
+    val obj = Gson().fromJson(formData, List::class.java)
+
+    // Navigates the nested arrays using given sequence
+    var curr = obj
+    for (i in APPROVERS_UNWRAP_SEQUENCE) {
+        curr = curr[i] as List<*>
+    }
+
+    val approvers = ArrayList<String>(curr.size)
+    for (approver in curr) {
+        if (approver !is String) continue
+
+        approvers.add(approver)
+    }
+
+    return SPFormData(
+            obj[REQUEST_UUID] as String,
+            obj[TIMESTAMP_ZEROED] as String,
+            obj[CANARY] as String,
+            approvers,
+            obj
+    )
+}
 
 class KBBackend : SPBackend {
 
@@ -38,7 +92,7 @@ class KBBackend : SPBackend {
             Utils.debugLog("\nGET RESP BODY:")
             Utils.debugLog(body)
 
-            return SharePointPostback.parseFormData(body)
+            return parseFormData(body)
         }
 
     @Throws(IOException::class)
